827ffb8e81f79cf320954a1230f7ef4da604d491
Alex Restifo <calix1999@gmail.com>
2019-05-09 02:01:53 -0500

    first commit

--- quiz1.php
+++ quiz1.php
@@ -1,15 +1,24 @@
 <?php
+// Set up database connection
 $dbc = mysqli_connect("localhost", "root", "password", "cwulf");
 
+// Checks if the database connected properly
 if (!$dbc) {
     echo "Failed to connect to the database. Error = " . mysqli_connect_error();
 }
 
+// Query to get the question and answerChoices from the database
 $query = "SELECT question, answerChoices FROM quiz1";
+
+// Run whatever is in this if statement if the query executed successfully
 if ($result = mysqli_query($dbc, $query)) {
     // Build question and answer section for each question
     while ($row = $result->fetch_assoc()) {
-        echo $row["question"] . "  " . $row["answerChoices"];
+        $questionText = $row["question"];
+        $answerChoiceText = $row["answerChoices"];
+        $answerChoices = explode("|", $answerChoiceText);
+
+        // $answerChoices is now an array containing all the answer choices. Do some logic here to determine the user's "personality"
     }
 }
 
